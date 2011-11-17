e388967957bc543caed433488a71d6156f43d890
Scott Moore <scott@thinkmoore.net>
2011-11-16 19:58:32 -0500

    Added support for quantifiers, proved simple delegation proof

--- logic.v
+++ logic.v
@@ -1,18 +1,32 @@
-Require Import String.
 Require Import List.
+Require Import String.
+Require Import Peano_dec.
+Require Import Compare_dec.
 
-Definition var := string.
-Definition principal := string.
+Definition var := nat.
+Definition pcpl := string.
+
+Inductive principal : Type :=
+| Var_p : var -> principal
+| Pcpl_p : pcpl -> principal.
+
+Definition principal_dec : forall a b : principal, {a=b} + {a <> b}.
+generalize string_dec ;
+generalize eq_nat_dec ;
+decide equality.
+Defined.
 
 Inductive formula : Type :=
-| Pred_f : string -> var -> formula
+| Pred_f : string -> principal -> formula
 | Impl_f : formula -> formula -> formula
 | Signed_f : principal -> formula -> formula
 | Says_f : principal -> formula -> formula
-| Confirms_f : principal -> formula -> formula.
+| Confirms_f : principal -> formula -> formula
+| Abs_f : formula -> formula.
 
 Definition formula_dec : forall a b : formula, {a=b} + {a <> b}.
-generalize string_dec.
+generalize principal_dec ;
+generalize string_dec ;
 decide equality.
 Defined.
 
@@ -26,15 +40,72 @@ Fixpoint member (f:formula) (C:context) : Prop :=
 
 Reserved Notation "c '|-' g" (at level 70).
 
+Fixpoint subst (a:pcpl) (f:formula) (n:nat) : formula :=
+  match f with
+    | Pred_f s (Var_p v) => if eq_nat_dec v n 
+                              then Pred_f s (Pcpl_p a)
+                              else if gt_dec v n
+                                then Pred_f s (Var_p (v -1))
+                                else Pred_f s (Var_p v)
+    | Pred_f s (Pcpl_p p) => Pred_f s (Pcpl_p p)
+    | Impl_f f1 f2 => Impl_f (subst a f1 n) (subst a f2 n)
+    | Signed_f (Var_p v) f => if eq_nat_dec v n
+                                then Signed_f (Pcpl_p a) (subst a f n)
+                                else if gt_dec v n
+                                  then Signed_f (Var_p (v-1)) (subst a f n)
+                                  else Signed_f (Var_p v) (subst a f n)
+    | Signed_f (Pcpl_p p) f => Signed_f (Pcpl_p p) (subst a f n)
+    | Says_f (Var_p v) f => if eq_nat_dec v n
+                             then Says_f (Pcpl_p a) (subst a f n)
+                             else if gt_dec v n
+                               then Says_f (Var_p (v-1)) (subst a f n)
+                               else Says_f (Var_p v) (subst a f n)
+    | Says_f (Pcpl_p p) f => Says_f (Pcpl_p p) (subst a f n)
+    | Confirms_f (Var_p v) f => if eq_nat_dec v n
+                                  then Confirms_f (Pcpl_p a) (subst a f n)
+                                  else if gt_dec v n
+                                    then Confirms_f (Var_p (v-1)) (subst a f n)
+                                    else Confirms_f (Var_p v) (subst a f n)
+    | Confirms_f (Pcpl_p p) f => Confirms_f (Pcpl_p p) (subst a f n)
+    | Abs_f f => Abs_f (subst a f (n+1))
+  end.
+
 Inductive entails : context -> formula -> Prop :=
 | Init_e : forall C f, member f C -> C |- f
-| Tauto_e : forall C f, C |- f -> forall a, C |- Says_f a f
-| Weaken_e : forall C f1 f2, f1::C |- f2 -> C |- Impl_f f1 f2
-| Impl_e : forall C f1 f2 f3, C |- f1 -> f2::C |- f3 -> (Impl_f f1 f2)::C |- f3
-| Signed_e : forall C a f1 f2, f1::C |- Says_f a f2 -> (Signed_f a f1)::C |- Says_f a f2
-| Confirms_e : forall C a f1 f2, f1::C |- Says_f a f2 -> (Confirms_f a f1)::C |- Says_f a f2
-| Says_e : forall C a f1 f2, f1::C |- Says_f a f2 -> (Says_f a f1)::C |- Says_f a f2
+| Tauto_e : forall C f, C |- f -> 
+                forall a, C |- Says_f a f
+| Weaken_e : forall C f1 f2, f1::C |- f2 -> 
+                                   C |- Impl_f f1 f2
+| Impl_e : forall C f1 f2 f3, C |- f1 -> 
+                                  f2::C |- f3 ->
+                                    (Impl_f f1 f2)::C |- f3
+| Signed_e : forall C a f1 f2, f1::C |- Says_f a f2 -> 
+                                   (Signed_f a f1)::C |- Says_f a f2
+| Confirms_e : forall C a f1 f2, f1::C |- Says_f a f2 -> 
+                                     (Confirms_f a f1)::C |- Says_f a f2
+| Says_e : forall C a f1 f2, f1::C |- Says_f a f2 ->
+                                 (Says_f a f1)::C |- Says_f a f2
+| Spec_e : forall C p f1 f2 a, (subst p f1 0)::C |- Says_f a f2 ->
+                                 (Abs_f f1)::C |- Says_f a f2
 where "C '|-' F" := (entails C F).
+
+Definition delegate a b p := Signed_f (Pcpl_p a) (Abs_f (Impl_f (Says_f (Pcpl_p b) (Pred_f p (Var_p 0))) (Pred_f p (Var_p 0)))).
+
+Open Local Scope string_scope.
+
+Lemma test : (delegate "a" "b" "ok")::(Signed_f (Pcpl_p "b") (Pred_f "ok" (Pcpl_p "c")))::nil |- Says_f (Pcpl_p "a") (Pred_f "ok" (Pcpl_p "c")).
+Proof.
+  unfold delegate.
+  eapply Signed_e.
+  eapply (Spec_e _ "c" _ _ _).
+  simpl.
+  eapply Impl_e.
+  eapply Signed_e.
+  eapply Tauto_e.
+  econstructor ; simpl; auto.
+  eapply Tauto_e.
+  econstructor ; simpl; auto.
+Qed.
 
 (** Define a data structure for proofs *)
 
