148c01e975d7063cca5c9562b58abbf430b71cc3
Scott Moore <sdmoore@fas.harvard.edu>
2011-11-20 04:37:42 -0500

    Changed Says_Spec to work more like a Says rule, proved simple delegation

--- logic.v
+++ logic.v
@@ -73,7 +73,7 @@ Inductive entails : context -> formula -> Prop :=
 | Says_e : forall C a f1 f2, f1::C |-- Says_f a f2 ->
                                  (Says_f a f1)::C |-- Says_f a f2
 | Spec_e : forall C p f1 f2 a, (subst f1 0 p)::C |-- Says_f a f2 ->
-                                 (Abs_f f1)::C |-- Says_f a f2
+                                 (Says_f a (Abs_f f1))::C |-- Says_f a f2
 | Weaken_e : forall C1 C2 f, C1 |-- f -> incl C1 C2 -> C2 |-- f (* should prove as a lemma? *)
 where "C '|--' F" := (entails C F).
 
@@ -106,7 +106,7 @@ Inductive proof : Set :=
  | Says_Confirms_r : formula -> proof -> proof -> proof (* formula = Says_f a f2, proof of Confirms_f a f1, Says_f a f2 given f1*) 
  | Says_Signed_r : formula -> proof -> proof -> proof (* formula = Says_f a f2, proof of Signed_f a f1, Says_f a f2 given f1*) 
  | Says_Says_r : formula -> proof -> proof -> proof (* formula = Says_f a f2, proof of Says_f a f1, Says_f a f2 given f1*) 
- | Says_Spec_r : formula -> pcpl -> proof -> proof -> proof. (* formula = Says_f a f2, principal = p, proof of Abs_f f1, Says_f a f2 given subst f1 0 p *)
+ | Says_Spec_r : formula -> pcpl -> proof -> proof -> proof. (* formula = Says_f a f2, principal = p, proof of Says_f a Abs_f f1, Says_f a f2 given subst f1 0 p *)
 
 Definition proof_goal p : formula :=
 match p with
@@ -189,6 +189,20 @@ match p with
   | Says_Says_r f p1 p2 => List.app (axioms p1 e) (axioms p2 e)
   | Says_Spec_r f a p1 p2 => List.app (axioms p1 e) (axioms p2 e)
   | Assump_r _ => nil
+end.
+
+Fixpoint assumps (p:proof) : context :=
+match p with
+  | Signed_r f => nil
+  | Confirms_r f => nil
+  | Tauto_r f p1 => assumps p1
+  | Weaken_Impl_r f p1 => assumps p1
+  | Impl_r f p1 p2 => List.app (assumps p1) (assumps p2)
+  | Says_Confirms_r f p1 p2 => List.app (assumps p1) (assumps p2)
+  | Says_Signed_r f p1 p2 => List.app (assumps p1) (assumps p2)
+  | Says_Says_r f p1 p2 => List.app (assumps p1) (assumps p2)
+  | Says_Spec_r f a p1 p2 => List.app (assumps p1) (assumps p2)
+  | Assump_r f => f::nil
 end.
 
 Lemma subst_p_prsubst : forall p1 p2 e, (subst_p (prsubst p2 (None::e)) 0 p1) = (prsubst p2 ((Some p1)::e)).
@@ -289,9 +303,11 @@ Fixpoint check (g:formula) (e:env) (p:proof) (c:context) : option (axioms p e ++
         | Says_Spec_r (Says_f pr f2) theP p1 p2 =>
              if formula_comp f e (Says_f pr f2) e
                then match proof_goal p1 with
-                      | Abs_f f1 => if check (Abs_f f1) e p1 c
-                                      then if check f e p2 ((subst (subst_simul f e) 0 (theP))::c) then Some _ else None
-                                      else None
+                      | Says_f pr0 (Abs_f f1) => if principal_dec (prsubst pr e) (prsubst pr0 e)
+                                                then if check (Says_f pr0 (Abs_f f1)) e p1 c
+                                                  then if check f e p2 ((subst (subst_simul f1 e) 0 (theP))::c) then Some _ else None
+                                                  else None
+                                                else None
                       | _ => None
                     end
                else None
@@ -344,11 +360,12 @@ Fixpoint check (g:formula) (e:env) (p:proof) (c:context) : option (axioms p e ++
     apply incl_cheat.
     apply incl_cheat.
   (* Says_Spec *)
-    eapply (Weaken_e _ ((axioms p1 e ++ axioms p2 e) ++ c)) in _H0.
-    eapply (cut_elimination _ (Abs_f (subst_simul f1 (None :: e)))) ; auto.
+    rewrite _H0 in *.
+    eapply (Weaken_e _ ((axioms p1 e ++ axioms p2 e) ++ c)) in _H1.
+    eapply (cut_elimination _ (Says_f (prsubst pr0 e) (Abs_f (subst_simul f3 (None :: e))))) ; auto.
     eapply (Spec_e _ theP).
-    rewrite subst_subst_simul.
-    eapply (Weaken_e _ (subst_simul f1 (Some theP :: e) :: (axioms p1 e ++ axioms p2 e) ++ c)) in _H1 ; auto.
+    rewrite subst_subst_simul in *.
+    eapply (Weaken_e _ (subst_simul f3 (Some theP :: e) :: (axioms p1 e ++ axioms p2 e) ++ c)) in _H2 ; auto.
     apply incl_cheat.
     apply incl_cheat.
 Defined.
@@ -358,39 +375,47 @@ Admitted.
 
 (** Testing *)
 
-Definition delegate a b p := Signed_f (Pcpl_p a) (Abs_f (Impl_f (Says_f (Pcpl_p b) (Pred_f p (Var_p 0))) (Pred_f p (Var_p 0)))).
+Definition delegate b p := Abs_f (Impl_f (Says_f (Pcpl_p b) (Pred_f p (Var_p 0))) (Pred_f p (Var_p 0))).
+Definition approve c p := (Pred_f p (Pcpl_p c)).
+
+Definition delegate_signed a b p := Signed_f (Pcpl_p a) (delegate b p).
 
 Open Scope string_scope.
 
-Lemma test : (delegate "a" "b" "ok")::(Signed_f (Pcpl_p "b") (Pred_f "ok" (Pcpl_p "c")))::nil |-- Says_f (Pcpl_p "a") (Pred_f "ok" (Pcpl_p "c")).
-Proof.
-  Show.
-  unfold delegate ; repeat prove_auth; spec_auth "c" ; repeat prove_auth.
-Qed.
+Definition signed sayer f := Signed_r (Signed_f sayer f).
 
-Lemma says : Signed_f (Pcpl_p "a") (Pred_f "p" (Pcpl_p "a"))::nil  |-- Says_f (Pcpl_p "a") (Pred_f "p" (Pcpl_p "a")).
-Proof.
- Show.
- apply Signed_e.
- apply Tauto_e.
- apply Init_e.
-Qed.
+Eval simpl in check (Signed_f (Pcpl_p "A") (Pred_f "ok" (Pcpl_p "B"))) nil (signed (Pcpl_p "A") (Pred_f "ok" (Pcpl_p "B"))) nil.
 
+Definition says_from_assump sayer f := Tauto_r (Says_f (Pcpl_p sayer) f) (Assump_r f).
+Definition says_from_signed sayer f := Says_Signed_r (Says_f (Pcpl_p sayer) f) (signed (Pcpl_p sayer) f) (says_from_assump sayer f).
 
-Print formula.
+Eval simpl in check (Says_f (Pcpl_p "A") (Pred_f "ok" (Pcpl_p "B"))) nil (says_from_signed "A" (Pred_f "ok" (Pcpl_p "B"))) nil.
 
-Definition signed sayer pred p := let thepred := Pred_f pred p in (Signed_r (Signed_f sayer thepred)).
+Definition delegate_from_signed a b p := Signed_r (delegate_signed a b p). 
 
-Eval simpl in check (Signed_f (Pcpl_p "A") (Pred_f "ok" (Pcpl_p "B"))) nil (signed (Pcpl_p "A") "ok" (Pcpl_p "B")) nil.
+Definition valid proof := check (proof_goal proof) nil proof (assumps proof).
 
-Definition says_from_assump sayer pred p := let thepred := Pred_f pred p
-                                             in Tauto_r (Says_f sayer thepred) (Assump_r (thepred)).
+Eval simpl in let p := "ok" in
+              let a := "A" in
+              let b := "B" in
+              let c := "C" in
+              let bapprove := says_from_signed b (approve c p) in
+              let assumed := Assump_r (Impl_f (Says_f (Pcpl_p b) (Pred_f p (Pcpl_p c))) (Pred_f p (Pcpl_p c))) in
+              let proof := (Impl_r (Pred_f p (Pcpl_p c)) bapprove  assumed) in
+                check (proof_goal proof) nil proof (assumps assumed).
 
-Eval simpl in check (Says_f (Pcpl_p "A") (Pred_f "ok" (Pcpl_p "B"))) nil (says_from_assump (Pcpl_p "A") "ok" (Pcpl_p "B")) ((Pred_f "ok" (Pcpl_p "B"))::nil).
+Definition approval_proof b c p := says_from_signed b (approve c p).
+Definition delegation_proof a b p := says_from_signed a (delegate b p).
 
-Definition says_from_signed sayer pred p := let thepred := Pred_f pred p
-                                             in Says_Signed_r (Says_f sayer thepred) (signed sayer pred p) (says_from_assump sayer pred p).
+Definition use_delegation (a b c:pcpl) (p:string) (dp ap:proof) :=
+  Says_Spec_r (Says_f (Pcpl_p a) (approve c p)) c dp (Tauto_r (Says_f (Pcpl_p a) (approve c p))
+    (Impl_r (approve c p) ap (Assump_r (Impl_f (Says_f (Pcpl_p b) (Pred_f p (Pcpl_p c))) (Pred_f p (Pcpl_p c)))))).
 
-Check says_from_signed.
-
-Eval simpl in check (Says_f (Pcpl_p "A") (Pred_f "ok" (Pcpl_p "B"))) nil (says_from_signed (Pcpl_p "A") "ok" (Pcpl_p "B")) nil.
+Eval simpl in let p := "ok" in
+              let a := "A" in
+              let b := "B" in
+              let c := "C" in
+              let dp := delegation_proof a b p in
+              let ap := approval_proof b c p in
+              let ud := use_delegation a b c p dp ap in
+                check (proof_goal ud) nil ud nil.
