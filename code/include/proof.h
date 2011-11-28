3b32677b6c0a4be8a70aca9f17bed182a59821f8
Scott Moore <scott@thinkmoore.net>
2011-11-28 17:10:28 -0500

    delegation test case

--- code/include/proof.h
+++ code/include/proof.h
@@ -139,4 +139,14 @@ Proof proof_says_signed(Formula goal, Proof proof1, Proof proof2);
 Proof proof_says_says(Formula goal, Proof proof1, Proof proof2);
 Proof proof_says_spec(Formula goal, Pcpl p, Proof proof1, Proof proof2);
 
+Proof signed_proof(Pcpl sayer, Formula f);
+Proof says_from_assump(Pcpl sayer, Formula f);
+Proof says_from_signed(Pcpl sayer, Formula f);
+Formula approve(Pcpl pcpl, Predicate pred);
+Proof approval_from_signed(Pcpl approver, Predicate pred, Pcpl pcpl);
+Formula delegate(Pcpl pcpl, Predicate pred);
+Formula delegate_signed(Pcpl a, Pcpl b, Predicate p);
+Proof delegate_from_signed(Pcpl a, Pcpl b, Predicate p);
+Proof use_delegation(Pcpl a, Pcpl b, Pcpl c, Predicate p, Proof dpf, Proof apf);
+
 #endif
