ee9ecab3215b9495688f1de3e3172f6838667ac3
Andrew Johnson <ajohnson@seas.harvard.edu>
2011-11-28 15:02:27 -0500

    proof printing

--- code/main.c
+++ code/main.c
@@ -53,6 +53,73 @@ void print_abs(){
   formula_print(abs);
 }
 
+void print_signed_p(){
+  Principal pcpl = principal_pcpl(A);
+  Formula pred = formula_pred(OK, pcpl);
+  Proof signedp = proof_signed(pred);
+  proof_print(signedp);
+}
+
+void print_confirms_p() {
+	Principal pcpl = principal_pcpl(A);
+	Formula pred = formula_pred(OK, pcpl);
+	Proof confirms = proof_confirms(pred);
+	proof_print(confirms);
+}
+
+void print_assump_p(){
+	Principal pcpl = principal_pcpl(A);
+	Formula pred = formula_pred(OK, pcpl);
+	Proof assump = proof_assump(pred);
+	proof_print(assump);
+}
+
+void print_tauto_p() {
+	Principal pcpl1 = principal_pcpl(A);
+	Formula f1 = formula_pred(OK, pcpl1);
+	Principal pcpl2 = principal_pcpl(B);
+	Formula says = formula_says(pcpl2, f1);
+
+	Proof assump = proof_assump(f1);
+
+	Proof tauto = proof_tauto(says, assump);
+	proof_print(tauto);
+}
+
+void print_weaken_impl_p() {
+	Principal pcpl1 = principal_pcpl(A);
+	Formula f1 = formula_pred(OK, pcpl1);
+	Principal pcpl2 = principal_pcpl(B);
+	Formula f2 = formula_pred(ALRIGHT, pcpl2);
+	Formula impl = formula_impl(f1, f2);
+
+	Proof assump = proof_assump(f1);
+
+	Proof weaken = proof_weaken_impl(impl, assump);
+	proof_print(weaken);
+}
+
+void print_impl_p(){
+	Principal pcpl1 = principal_pcpl(A);
+	Formula f1 = formula_pred(OK, pcpl1);
+	Principal pcpl2 = principal_pcpl(B);
+	Formula f2 = formula_pred(ALRIGHT, pcpl2);
+	Formula impl = formula_impl(f1, f2);
+
+	Proof assump1 = proof_assump(f1);
+	Proof assump2 = proof_assump(impl);
+
+	Proof impl_p = proof_impl(f1, assump1, assump2);
+}
+
+//void print_says_confirms(){
+//	Principal pcpl1 = principal_pcpl(A);
+//	Formula f2 = formula_pred(OK, pcpl1);
+//	Formula says = formula_says(pcpl1, f12);
+//
+//
+//}
+
 int main()
 {
 	print_pred();
